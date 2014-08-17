56d320eb858ce925d0e6aaea30ebcaca3fead87c
Liangzhao Wu <wuliangzhao10@gmail.com>
2014-08-17 20:42:46 +0800

    update .

--- Evaler/Program.cs
+++ Evaler/Program.cs
@@ -9,45 +9,138 @@ namespace Evaler
     {
         static void Main(string[] args)
         {
-            //(op e1 e2)
-            //(+ 1 2)
-            //Console.WriteLine(Value("(+ 1 2)"));
-            //Console.WriteLine(car("(+ 1 2)") + "|");
-            Console.WriteLine(car("()") + "|");
+            Test.Run();
             Console.Read();
         }
 
         static int Value(string exp)
         {
-            return 0;
+            string type = Type(exp);
+
+            switch (type)
+            {
+                case "int":
+                    return int.Parse(exp);
+                case "exp":
+                    return Apply(car(exp), car(cdr(exp)), car(cdr(cdr(exp))));
+                default:
+                    return 0;
+            }
+        }
+
+        static int Apply(string op, string op1, string op2)
+        {
+            switch (op)
+            {
+                case "+":
+                    return Value(op1) + Value(op2);
+                case "-":
+                    return Value(op1) - Value(op2);
+                default:
+                    return 0;
+            }
+        }
+
+        static string Type(string exp)
+        {
+            if (exp.StartsWith("("))
+                return "exp";
+            //else if (exp == "t" || exp == "f")
+            //    return "bool";
+            else if (exp == "+" || exp == "-")
+                return "op";
+            else
+                return "int";
         }
 
         static string car(string exp)
         {
-            if (string.IsNullOrEmpty(exp) || exp == "()")
-                return string.Empty;
+            if (exp == "()" || string.IsNullOrEmpty(exp))
+                return "null";
+            else if (exp[1] == '(')
+                return FirstList(exp);
             else
-                return exp.Substring(1, exp.IndexOf(' ') - 1);
+                return FirstItem(exp);
+        }
+
+        private static string FirstItem(string exp)
+        {
+            string result = string.Empty;
+
+            for (int i = 1; i < exp.Length; i++)
+            {
+                if (exp[i] == '(' || exp[i] == ')' || exp[i] == ' ')
+                {
+                    result = exp.Substring(1, i - 1);
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        private static string FirstList(string exp)
+        {
+            string result = string.Empty;
+            int cnt = 1;
+            for (int i = 2; i < exp.Length; i++)
+            {
+                if (exp[i] == '(')
+                    cnt++;
+
+                if (exp[i] == ')')
+                    cnt--;
+
+
+                if (cnt == 0)
+                {
+                    result = exp.Substring(1, i);
+                    break;
+                }
+            }
+
+            return result;
         }
 
         static string cdr(string exp)
         {
-            return string.Empty;
+            return car(exp) == "null" ?
+                "()" :
+                "(" + exp.Substring(car(exp).Length + 1).TrimStart();
         }
 
-        //static string Left(string exp)
-        //{
-        //    return "";
-        //}
+        class Test
+        {
+            public static void TestCar()
+            {
+                string exp;
+                exp = "()";
+                //exp = "(aaa)";
+                //exp = "(())";
+                //exp = "(a b)";
+                //exp = "((a) b)";
+                //exp = "(a b c)";
+                //exp = "((a) b c)";
+                //exp = "(() b c)";
+                //exp = "(()())";
+                //exp = "(())";
+                //Console.WriteLine(cdr(exp) + "|");
+                Console.WriteLine(car(exp) + "|");
+            }
 
-        //static string Right(string exp)
-        //{
-        //    return "";
-        //}
+            public static void TestValue()
+            {
+                string exp =
+                    //"(+ 1 2)";
+                    //"(+ (+ 1 2) 2)";
+                    "(+ (+ 1 2) (+ 3 2))";
+                Console.WriteLine(Program.Value(exp) + "|");
+            }
 
-        //static string Op(string exp)
-        //{
-        //    return "+";
-        //}
+            public static void Run()
+            {
+                TestValue();
+            }
+        }
     }
 }
